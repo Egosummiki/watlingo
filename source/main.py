36122f875248256f6b35182ddd7f20a8f80da032
Mikołaj Bednarek <egosummiki@gmail.com>
2018-10-29 10:23:46 +0100

    INIT: Added main source file.

---
+++ source/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+##### Wat Lingo #####
+
+# Projekt na metodyki wytwarzania oprogramowania.
+
+# Main program file.
+# Initialisation of the whole program.
+
+# Data: 29/10/2018
+# Author: Mikołaj Bednarek
+
+if __name__ == '__main__':
+
+#Leave it for now
+    pass 
